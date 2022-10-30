84e35d3c00fb5166e9b25a460e94e2702f6498c2
Salvador Gonzalez <gonzalezlsalvador@outlook.com>
2022-10-30 07:29:41 -0700

    Infraestructure project added

---
+++ LibraryManagementSystem.Infrastructure/ApiCaller.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Infrastructure
+{
+    public class ApiCaller<T> where T : class
+    {
+        HttpClient client;
+        private readonly string _baseUrl = "https://reqres.in/api/";
+
+        public ApiCaller()
+        {
+            client = new HttpClient();
+        }
+
+        public async Task<T> SendRequest(string resource)
+        {
+            var response = await client.GetAsync(_baseUrl + resource);
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<T>(content);
+
+            return result;
+        }
+
+        public async Task<string> SendPostRequest(string resource, object body)
+        {
+            var json = JsonConvert.SerializeObject(body);
+            var content = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+
+            var response = await client.PostAsync(_baseUrl + resource, content);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ LibraryManagementSystem.Infrastructure/Clients/BookApiClient.cs
@@ -0,0 +1,20 @@
+﻿using LibraryManagementSystem.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Infrastructure.Clients
+{
+    internal class BookApiClient : IBookApi
+    {
+        ApiCaller<object> api = new ApiCaller<object>();
+
+        public async Task<List<BookDTO>> GetBooksAsync()
+        {
+            var response = await api.SendRequest("users/2");
+            return new List<BookDTO>();
+        }
+    }
+}
