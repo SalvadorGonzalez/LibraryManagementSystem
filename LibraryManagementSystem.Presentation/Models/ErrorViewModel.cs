d0dfb70692c866b116a446e812f9d88bf511e632
Salvador Gonzalez <gonzalezlsalvador@outlook.com>
2022-10-30 04:25:57 -0700

    Add project files.

---
+++ LibraryManagementSystem.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibraryManagementSystem.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
